8f76d5c1541894e78afc49dd76fb6d7d62b5f197
Scott Hawley <iamultima0@gmail.com>
2022-10-15 19:56:56 -0700

    pushing changers to kernal

--- OsSimulation/Kernal.java
+++ OsSimulation/Kernal.java
@@ -5,21 +5,15 @@ public class Kernal {
     //the main method to run the kernel simulation
     public static void main(String[] args) {
 
-        try {
-            manager.main();
-            Thread.sleep(41000);
-            shared.main();
-        }
-        catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
-
+        //run network simulation
         Network client = new Network("whois.internic.net", 43, "google.com");
         Network client2 = new Network("google.com", 80);
 
+        //run file simulation
         FileManager fileManager = new FileManager();
         fileManager.create("hello.txt", "OsSimulation/hello.txt");
 
+        //run secondary storage (disk) simulation
         StorageController storageController = new StorageController();
 
         for (int i = 0; i < 5; i++) {
@@ -38,6 +32,15 @@ public class Kernal {
         storageController.setSecondaryStorage(2, memoryObject2);
         storageController.setSecondaryStorage(3, memoryObject3);
 
+        //run process and scheduler simulation
+        try {
+            manager.main();
+            Thread.sleep(41000);
+            shared.main();
+        }
+        catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
     }
 
 }
