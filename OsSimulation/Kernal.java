aef75dd164bf5815efa268ff59ec551d9a720f2d
Scott Hawley <iamultima0@gmail.com>
2022-10-15 19:11:48 -0700

    Update Kernal.java

--- OsSimulation/Kernal.java
+++ OsSimulation/Kernal.java
@@ -5,12 +5,6 @@ public class Kernal {
     //the main method to run the kernel simulation
     public static void main(String[] args) {
 
-
-        Network server = new Network(5000);
-        NetworkClient client = new NetworkClient("127.0.0.1", 5000);
-
-        /*
-
         try {
             manager.main();
             Thread.sleep(41000); 
@@ -20,7 +14,12 @@ public class Kernal {
             Thread.currentThread().interrupt();
         }
 
-*/
+        Network client = new Network("whois.internic.net", 43, "google.com");
+        Network client2 = new Network("google.com", 80);
+
+        FileManager fileManager = new FileManager();
+        fileManager.create("hello.txt", "OsSimulation/hello.txt");
+
     }
 
 }
