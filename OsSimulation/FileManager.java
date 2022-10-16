f38f64ef90b8e09b42df197cd578604695158923
Scott Hawley <iamultima0@gmail.com>
2022-10-15 19:29:04 -0700

    pushing final changes to file manager

--- OsSimulation/FileManager.java
+++ OsSimulation/FileManager.java
@@ -17,19 +17,19 @@ public class FileManager {
     }
     void create(String filename, String fileurl){
         File obj = new File("myfile.txt");
+        try {
+            if (obj.createNewFile()) {
+                System.out.println("File created: " + obj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
         Index = files.size();
         data = new FileData(obj, filename, fileurl, Index);
         files.add(Index, data);
 
         files.get(Index).printData();
     }
-
-    void open() {
-
-    }
-
-    void close() {
-
-    }
-
 }
